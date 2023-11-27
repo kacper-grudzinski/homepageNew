f539fe998e185a9c6aa1899b096c96f006151fd5
Kacper Grudziński <kgrvdzin@gmail.com>
2023-11-27 14:26:37 +0100

    lists and tables

--- main.js
+++ main.js
@@ -4,14 +4,39 @@ const age = 19
 console.log(firstName, age)
 console.log(`Nazywam się ${firstName} i mam ${age} lat.`)
 
-alert('Cześć!');
-
 console.log('Cześć!')
 
 const header = document.querySelector('.page-header__heading--js');
 
-header.innerHTML = `Nazywam się ${firstName} i mam ${age} lat.`;
-
 console.log(header.style);
 
 header.style.color = 'red'
+
+const calculate = myNumber => (myNumber+4)*7;
+
+calculate(1);
+
+const result = calculate(1);
+console.log(result);
+
+const deathstar = {
+    diameter: 120000,
+    fire: (target) => {
+        console.log(`${target} destroyed 💥`)
+    },
+    isOperating: true, 
+    levels:357,
+    name: 'Death Star',
+    population: 10000,
+    commander: {
+        name: 'Darth Vader'
+    }
+}
+
+console.log(deathstar.commander.name);
+
+console.log(deathstar['diameter']);
+
+console.log(deathstar.name);
+
+deathstar.fire('kolins')
