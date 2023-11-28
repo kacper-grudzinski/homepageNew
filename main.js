d9c6f23ef9bca6fc77d6373d73360f0f0aeb4000
Kacper Grudziński <kgrvdzin@gmail.com>
2023-11-28 19:22:34 +0100

    new background

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const firstName = 'Kacper';
-const age = 19
+const firstName = "Kacper";
+const age = 19;
 
-console.log(firstName, age)
-console.log(`Nazywam się ${firstName} i mam ${age} lat.`)
+console.log(firstName, age);
+console.log(`Nazywam się ${firstName} i mam ${age} lat.`);
 
-console.log('Cześć!')
+console.log("Cześć!");
 
-const header = document.querySelector('.page-header__heading--js');
+const header = document.querySelector(".page-header__heading--js");
 
 console.log(header.style);
 
-header.style.color = 'red'
+header.style.color = "red";
 
-const calculate = myNumber => (myNumber+4)*7;
+const calculate = (myNumber) => (myNumber + 4) * 7;
 
 calculate(1);
 
@@ -20,23 +20,23 @@ const result = calculate(1);
 console.log(result);
 
 const deathstar = {
-    diameter: 120000,
-    fire: (target) => {
-        console.log(`${target} destroyed 💥`)
-    },
-    isOperating: true, 
-    levels:357,
-    name: 'Death Star',
-    population: 10000,
-    commander: {
-        name: 'Darth Vader'
-    }
-}
+  diameter: 120000,
+  fire: (target) => {
+    console.log(`${target} destroyed 💥`);
+  },
+  isOperating: true,
+  levels: 357,
+  name: "Death Star",
+  population: 10000,
+  commander: {
+    name: "Darth Vader",
+  },
+};
 
 console.log(deathstar.commander.name);
 
-console.log(deathstar['diameter']);
+console.log(deathstar["diameter"]);
 
 console.log(deathstar.name);
 
-deathstar.fire('kolins')
+deathstar.fire("kolins");
